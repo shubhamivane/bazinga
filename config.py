4a81a76e666f49d6a5be593cec0f240dbda736e2
shubhamivane <ivaneshubham@gmail.com>
2019-08-13 02:25:10 +0530

    db uri changed

--- config.py
+++ config.py
@@ -9,5 +9,5 @@ if os.path.isfile(dotenv_file):
 DEBUG = False
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = str(uuid.uuid4())
-SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+SQLALCHEMY_DATABASE_URI = 'postgres+psycopg2://lsftwfubnhqqhk:a975fdf2b3aa7e1489b4065565b47e08bd98f4c2c4b384e83e9629bdfb487e24@ec2-54-225-119-13.compute-1.amazonaws.com:5432/db44brfhai45f'
 SHOPIFY_KEY = b'c8169a0fa2a87c528c856bc2e3d7f5f0837731683594364cbbc830883e7f8e6f'
