7dea2cb15207cb4137d166573fdf837ead9391bd
shubhamivane <ivaneshubham@gmail.com>
2019-08-13 02:16:00 +0530

    flask

--- config.py
+++ config.py
@@ -1,9 +1,13 @@
 import os
 import uuid
+import dotenv
 
-DEBUG = True
-SQLALCHEMY_TRACK_MODIFICATIONS = False
 BASE_DIR = os.path.abspath(os.path.dirname(__name__))
-SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://shubham:shubhamcs@127.0.0.1:5432/Order'
-SHOPIFY_KEY = b'c8169a0fa2a87c528c856bc2e3d7f5f0837731683594364cbbc830883e7f8e6f'
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
+DEBUG = False
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = str(uuid.uuid4())
+SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+SHOPIFY_KEY = b'c8169a0fa2a87c528c856bc2e3d7f5f0837731683594364cbbc830883e7f8e6f'
