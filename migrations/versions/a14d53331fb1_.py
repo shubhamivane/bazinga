6a6d8f3952c833b089ec77e5e791d2b4dc495e27
shubhamivane <ivaneshubham@gmail.com>
2019-08-13 02:41:04 +0530

    version

---
+++ migrations/versions/a14d53331fb1_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: a14d53331fb1
+Revises: 
+Create Date: 2019-08-13 02:40:27.404058
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a14d53331fb1'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
